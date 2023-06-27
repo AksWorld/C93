4c0b4c8c50ba4fc829faef7b6c5c7ce4f47a64b6
AksWorld <123278691+AksWorld@users.noreply.github.com>
2023-06-26 20:14:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function login(){
+    User_name=d/s/my-song-practiceocument.getElementById("user_name").value;
+    localStorage.setItem("User_name",User_name);
+    window.location = "kwitter.html";
+}
